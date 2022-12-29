2eb77528105930d79736d35fdbbe75c07f1e1280
yzheka <yzheka.m@gmail.com>
2022-12-29 12:28:22 +0200

    Initial commit

---
+++ library/src/main/java/com/github/yzheka/contextprovider/ContextProvider.kt
@@ -0,0 +1,43 @@
+package com.github.yzheka.contextprovider
+
+import android.annotation.SuppressLint
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+
+class ContextProvider:ContentProvider() {
+    override fun onCreate(): Boolean {
+        mContext=context!!
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? = null
+
+    override fun getType(uri: Uri): String? = null
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int = 0
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int = 0
+
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        private lateinit var mContext:Context
+
+        fun getContext():Context = mContext
+    }
+}
